5bc1c0e917235d4b976f6046e2c04f2929b11169
Danil <75833312+progbagger@users.noreply.github.com>
2020-12-30 00:31:57 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ ofstream fout("output.txt");
 int main()
 {
     fout << fixed << setprecision(8); // установка точности на 8 знаков после запятой
+    /*
     int n;
     fin >> n;
     double e, a, b;
@@ -17,6 +18,10 @@ int main()
     SLE System(n);
     fin >> System;
     System.Rchd3(e, x0, a, b);
+    */
+    Vector t{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    for (const auto& i : t)
+        fout << i << endl;
     fin.close();
     fout.close();
     return 0;
