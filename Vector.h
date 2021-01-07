ddd8ef47b650e4db578b60f73942872fb240e380
Danil <75833312+progbagger@users.noreply.github.com>
2021-01-08 00:38:09 +0700

    Удалён итератор и добавлена евклидова норма
    Итератор мешал корректно удалять элемент, а евклидова норма это то же самое, что модуль вектора, но пусть на всякий случай будет.

--- Vector.h
+++ Vector.h
@@ -7,67 +7,46 @@ using namespace std;
 class Vector // класс для работы с векторами
 {
     double* V;
-    size_t size;
+    size_t size; // размер вектора
 
     void rawClean();
     void rawCopy(const Vector&);
 
 public:
 
-    double* next(double*);
-
-    class Iterator
-    {
-        double* el;
-        Vector* belong;
-
-    public:
-
-        Iterator();
-        Iterator(double&, Vector&);
-        Iterator(const Iterator&);
-        ~Iterator();
-        Iterator& operator = (const Iterator&);
-
-        Iterator operator ++ (int);
-        Iterator operator ++ ();
-
-        bool operator == (const Iterator&);
-        bool operator != (const Iterator&);
-
-        double& operator * ();
-    };
-
-    Iterator begin();
-    Iterator end();
-
     Vector(const Vector&);
     Vector();
     ~Vector();
     Vector& operator = (const Vector&);
     Vector(const size_t&);
-    Vector(const size_t&, double*);
+    Vector(const size_t&, const double&);
+    Vector(const size_t&, const Vector&); // размер и вектор-массив
     Vector(const initializer_list<double>&);
     Vector& operator = (const initializer_list<double>&);
 
-    size_t getSize() const;
-    double& operator [](const int&);
-    double& get(const int&) const;
+    size_t getSize() const; // размер вектора
+    double& operator [] (const size_t&); // с возможностью изменения
+    double& get(const size_t&) const; // константный
 
-    double operator *(const Vector&);
-    Vector operator /(const double&);
-    Vector operator -(const Vector&);
-    Vector operator +(const Vector&);
-    bool operator !() const;
-    bool operator == (const Vector&);
-    bool operator != (const Vector&);
+    double operator * (const Vector&) const; // скалярное произведение
+    Vector operator / (const double&) const; // деление на константу
+    Vector& operator /= (const double&);
+    Vector& operator *= (const double&);
+    Vector operator - (const Vector&) const; // вычитание векторов
+    Vector& operator -= (const Vector&);
+    Vector operator + (const Vector&) const; // сложение векторов
+    Vector& operator += (const Vector&);
+    bool operator ! () const; // ненулевой вектор
+    bool operator == (const Vector&) const;
+    bool operator != (const Vector&) const;
 
-    double infNorm() const;
-    double abs() const;
-    void swap(const int& i1, const int& i2);
+    double infNorm() const; // норма на бесконечности
+    double euclidNorm() const; // евклидова норма
+    double abs() const; // модуль вектора
+    void swap(const size_t& i1, const size_t& i2); // поменять элементы местами
 
-    friend Vector operator * (const double&, const Vector&);
-    friend Vector operator * (const Vector&, const double&);
+    friend Vector operator * (const double&, const Vector&); // коммутативное умножение на константу
+    friend Vector operator * (const Vector&, const double&); // коммунативное умножение на константу
 
     // операторы ввода/вывода вектора
     friend istream& operator >> (std::istream&, Vector&);
