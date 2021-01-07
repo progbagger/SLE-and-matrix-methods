1ae36b96e6deeb950465c00926de5000841b56a5
Danil <75833312+progbagger@users.noreply.github.com>
2021-01-08 00:40:13 +0700

    Добавлены методы поиска собственных значений
    Также добавлен конструктор, позволяющий создать матрицу просто списком значений типа double.

--- Matrix.h
+++ Matrix.h
@@ -2,13 +2,14 @@
 #include "Vector.h"
 #include <fstream>
 #include <initializer_list>
+#include <iomanip>
 
 using namespace std;
 
 class Matrix // класс для работы с квадратными матрицами
 {
     double** M;
-    size_t size;
+    size_t size; // размер матрицы
 
     void rawClean();
     void rawCopy(const Matrix&);
@@ -20,33 +21,51 @@ public:
     ~Matrix();
     Matrix(const size_t&);
     Matrix(const initializer_list<Vector>&);
+    Matrix(const initializer_list<double>&);
     Matrix& operator = (const Matrix&);
     Matrix& operator = (const initializer_list<Vector>&);
+    Matrix& operator = (const initializer_list<double>&);
 
-    size_t getSize() const;
-    double*& operator [] (const int&); // с возможностью изменения
-    double*& get(const int&) const;
+    size_t getSize() const; // размер матрицы
+    double*& operator [] (const size_t&); // с возможностью изменения
+    double*& get(const size_t&) const; // константный
 
     // стандартные операторы для работы с матрицами
-    Matrix operator + (const Matrix&);
-    Matrix operator - (const Matrix&);
-    Matrix operator * (const Matrix&);
-    Matrix operator * (const double&);
-    Matrix operator !();
+    Matrix operator + (const Matrix&) const; // сложение матриц
+    Matrix& operator += (const Matrix&);
+    Matrix operator - (const Matrix&) const; // вычитание матриц
+    Matrix& operator -= (const Matrix&);
+    Matrix operator * (const Matrix&) const; // перемножение матриц
+    Matrix& operator *= (const Matrix&);
+    Matrix operator * (const double&) const; // умножение матрицы на константу, также описана коммутативность
+    Matrix& operator *= (const double&);
+    Matrix operator ! () const; // транспонирование
+
+    // сравнение матриц
     bool operator == (const Matrix&) const;
     bool operator != (const Matrix&) const;
 
-    void swap(const int&, const int&); // смена строк местами
+    void swap(const size_t&, const size_t&); // смена строк местами
     double det() const; // вычисление определителя матрицы
 
-    Matrix reflect(); // нахождение обратной матрицы методом Жордана-Гаусса
-    Matrix H(); // матрица отражений
-    Matrix diag(); // диагональная матрица
-    Matrix lowerTriangle(); // нижняя треугольная матрица
-    Matrix upperTriangle(); // верхняя треугольная матрица
+    Matrix reflect() const; // нахождение обратной матрицы методом Жордана-Гаусса
+    Matrix H() const; // матрица отражений
+    Matrix diag() const; // диагональная матрица
+    Matrix lowerTriangle() const; // нижняя треугольная матрица
+    Matrix upperTriangle() const; // верхняя треугольная матрица
+
+    // возвращает вектор с диагонали
+    Vector diagV() const;
+
+    // спектральная норма матрицы
+    double sNorm() const;
 
     // нахождение собственных чисел и собственных векторов
-    void QR();
+    Matrix Hausholder() const; // преобразование Хаусхолдера
+    Matrix Givens() const; // преобразование Гивенса
+    void QR(const double&) const; // QR-алгоритм
+
+    void RQI(const double&, const double&) const; // Обратные итерации со сдвигом с соотношением Рэлея
 
     // операторы ввода/вывода матрицы
     friend istream& operator >> (istream&, Matrix&);
