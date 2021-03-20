6fa736af6d5d8e83ff879de6bd82935c8c6d1105
Danil <75833312+progbagger@users.noreply.github.com>
2021-03-20 09:06:02 +0700

    Delete Analysis.h

--- Analysis.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "SLE.h"
-
-Vector Lagrange_interpolation_builder(const Vector&, const Vector&); // создание полинома Лагранжа
-void printPolynomial(const Vector&); // вывод полинома на экран
-double calculatePolynomial(const Vector&, const double); // вычислить значение полинома в точке
